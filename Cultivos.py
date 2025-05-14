5b6b7626904fa034becae054bf5c4a98e4b2cd59
Derlys <derlys.ricardor@pca.edu.co>
2025-05-14 14:00:05 -0500

    agregue un archivo

--- Cultivos.py
+++ Cultivos.py
@@ -16,7 +16,6 @@ def analizar_cultivos(matriz):
         print("No hay datos para analizar.")
         return
 
-    # 1. Año(s) con menor cantidad de hectáreas de sorgo
     min_sorgo = matriz[0][1]
     for fila in matriz:
         if fila[1] < min_sorgo:
@@ -26,13 +25,11 @@ def analizar_cultivos(matriz):
         if fila[1] == min_sorgo:
             anios_min_sorgo.append(fila[0])
 
-    # 2. Año(s) con igual cantidad de sorgo y maíz
     anios_igual = []
     for fila in matriz:
         if fila[1] == fila[2]:
             anios_igual.append(fila[0])
 
-    # 3. Año(s) con mayor incremento de sorgo respecto al año anterior
     max_incremento = None
     anios_max_incremento = []
     for i in range(1, n):
@@ -44,7 +41,6 @@ def analizar_cultivos(matriz):
         if incremento == max_incremento:
             anios_max_incremento.append(matriz[i][0])
 
-    # 4. Año(s) con mayor cantidad absoluta entre sorgo y maíz
     max_cereal = None
     anios_max_cereal = []
     for fila in matriz:
@@ -55,17 +51,14 @@ def analizar_cultivos(matriz):
         if fila[1] == max_cereal or fila[2] == max_cereal:
             anios_max_cereal.append(fila[0])
 
-    # Imprimir resultados
-    print("\n--- Resultados del Análisis ---")
-    print(f"Año(s) con menor cantidad de hectáreas de sorgo: {anios_min_sorgo}")
+    print("\n--- Resultados Año(s) ---")
+    print(f"Menos hectáreas de sorgo cultivadas: {anios_min_sorgo}")
     if anios_igual:
-        print(f"Año(s) con igual cantidad de sorgo y maíz: {anios_igual}")
+        print(f"Misma cantidad de hectáreas de maíz: {anios_igual}")
     else:
         print("Ningún año tuvo igual cantidad de sorgo y maíz.")
-    print(f"Año(s) con mayor incremento de sorgo respecto al año anterior: {anios_max_incremento}")
-    print(f"Año(s) con mayor cantidad absoluta entre sorgo y maíz: {anios_max_cereal}")
+    print(f"Misma cantidad de hectáreas de maíz: {anios_max_incremento}")
+    print(f"Mayor cantidad de hectáreas entre los dos cereales2: {anios_max_cereal}")
 
-# --- Ejecución principal ---
 matriz = ingresar_datos_cultivos()
 analizar_cultivos(matriz)
-print("\n--- Fin del Programa ---")
---
+++ cuadro_magico.py
@@ -0,0 +1,4 @@
+# Un cuadrado mágico de 3 x 3 es una matriz de 3 x 3 formada por números del 1 al 9, 
+# en la que la suma de sus filas, columnas y diagonales es idéntica. Desarrolla un algoritmo 
+# y un programa en python que permita ingresar un cuadrado por teclado y determinar si es mágico o no.
+#  El programa debe validar que los números ingresados estén dentro del rango de 1 a 9.
